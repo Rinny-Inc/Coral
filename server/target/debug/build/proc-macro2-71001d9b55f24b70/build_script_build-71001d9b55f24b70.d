2946b1a133749b44b589c48a41e49ed636cafff3
Noksio <noksiopro@gmail.com>
2024-06-30 00:24:43 -0400

    init

---
+++ server/target/debug/build/proc-macro2-71001d9b55f24b70/build_script_build-71001d9b55f24b70.d
@@ -0,0 +1,5 @@
+D:\Coding\RustyCauldron\server\target\debug\build\proc-macro2-71001d9b55f24b70\build_script_build-71001d9b55f24b70.exe: C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.86\build.rs
+
+D:\Coding\RustyCauldron\server\target\debug\build\proc-macro2-71001d9b55f24b70\build_script_build-71001d9b55f24b70.d: C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.86\build.rs
+
+C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.86\build.rs:
