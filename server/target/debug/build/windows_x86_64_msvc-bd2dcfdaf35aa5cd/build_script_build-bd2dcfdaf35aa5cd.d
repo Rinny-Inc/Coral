b892dd0bd51b7016a5fa5f855566abf6e868ea93
Noksio <noksiopro@gmail.com>
2024-06-30 23:21:13 -0400

    rebase

--- server/target/debug/build/windows_x86_64_msvc-bd2dcfdaf35aa5cd/build_script_build-bd2dcfdaf35aa5cd.d
+++
@@ -1,5 +0,0 @@
-D:\Coding\RustyCauldron\server\target\debug\build\windows_x86_64_msvc-bd2dcfdaf35aa5cd\build_script_build-bd2dcfdaf35aa5cd.exe: C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.5\build.rs
-
-D:\Coding\RustyCauldron\server\target\debug\build\windows_x86_64_msvc-bd2dcfdaf35aa5cd\build_script_build-bd2dcfdaf35aa5cd.d: C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.5\build.rs
-
-C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.5\build.rs:
--- server/target/debug/deps/unicode_ident-9a9e272f64f1976c.d
+++
@@ -1,8 +0,0 @@
-D:\Coding\RustyCauldron\server\target\debug\deps\libunicode_ident-9a9e272f64f1976c.rmeta: C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
-
-D:\Coding\RustyCauldron\server\target\debug\deps\libunicode_ident-9a9e272f64f1976c.rlib: C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
-
-D:\Coding\RustyCauldron\server\target\debug\deps\unicode_ident-9a9e272f64f1976c.d: C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
-
-C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs:
-C:\Users\Utilisateur\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs:
