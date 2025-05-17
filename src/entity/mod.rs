bfc852885ada48b104eac9166ce4fc41e4eee6f5
Noksio <noksiopro@gmail.com>
2025-05-16 23:50:22 -0400

    implement Tickable & Entity trait

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -1,6 +1,26 @@
 pub mod living;
 pub mod player;
 
-pub struct Entity {
+pub trait Tickable {
+    fn tick();
+}
 
+pub trait EntityTrait {
+    fn id(&self) -> u128;
+}
+
+pub struct Entity {
+    id: u128
+}
+
+impl Tickable for Entity {
+    fn tick() {
+        // TODO
+    }
+}
+
+impl EntityTrait for Entity {
+    fn id(&self) -> u128 {
+        self.id
+    }
 }
