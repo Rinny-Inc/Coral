ca1be2ac2a7625a8fdbcd023e53cb7ea9a4cc41b
N. <noksiopro@gmail.com>
2026-07-11 22:08:09 -0400

    trigger new LoC

--- coral-command/src/lib.rs
+++ coral-command/src/lib.rs
@@ -100,35 +100,6 @@ where
     Arc::new(move |ctx| Box::pin(f(ctx)))
 }
 
-pub fn version_command() -> Command {
-    Command {
-        name: "version",
-        aliases: vec!["ver"],
-        description: "Show Coral version",
-        usage: "/version",
-        handler: make_handler(|_| async move {
-            let msg = ChatAppender::new()
-                .add(ChatBuilder::new("This server is running ").color(ChatColor::White))
-                .add(
-                    ChatBuilder::new("Coral")
-                        .color(ChatColor::LightPurple)
-                        .bold()
-                        .click_url("https://github.com/Rinny-Inc/Coral")
-                        .hover_text("Open Coral Github Page"),
-                )
-                .add(ChatBuilder::new(" version ").color(ChatColor::White))
-                .add(
-                    ChatBuilder::new(format!("git-Coral-{}", env!("GIT_HASH")))
-                        .color(ChatColor::LightPurple)
-                        .hover_text("Git commit hash"),
-                )
-                .add(ChatBuilder::new(" (Implementing API version 1.8.x)").color(ChatColor::White))
-                .build();
-            CommandResult::Success(msg)
-        }),
-    }
-}
-
 pub fn list_command(player_registry: Arc<PlayerRegistry>) -> Command {
     Command {
         name: "list",
